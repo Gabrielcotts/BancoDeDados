a071076922521e75f4bbf9a5a62ba2c1155072f4
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-08-03 13:57:16 -0300

    Update 4 - select tb_endereco.sql

--- IntroduçãoBD/Z - Projetos de prática/db_clinica_medica/4 - select tb_endereco.sql
+++ IntroduçãoBD/Z - Projetos de prática/db_clinica_medica/4 - select tb_endereco.sql
@@ -1,2 +1,15 @@
 select * from tb_endereco;
 select * from tb_paciente_endereco;
+
+-- DISTINCT
+select distinct estado from tb_endereco;
+select distinct estado from tb_endereco order by estado;
+select distinct cidade from tb_endereco;
+select distinct cidade from tb_endereco order by cidade;
+
+-- GROUP BY
+-- 55  Quantidade de pacientes por estado. (preencher)
+-- 58 Quantidade por cidade. (preencher)
+
+-- HAVING
+-- Mostre apenas os estados que possuem mais de dois pacientes. (preencher)
